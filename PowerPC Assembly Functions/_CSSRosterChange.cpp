faa8cdd323ca165b6b7ef69501abe7dd1524386f
QuickLava <61454308+QuickLava@users.noreply.github.com>
2022-12-08 21:36:39 -0800

    Last L-Load Code Rolled Into Code Menu
    All of the L-Load codes are now controlled internally by the code menu, and are fully roster-sensitive.

--- PowerPC Assembly Functions/_CSSRosterChange.cpp
+++ PowerPC Assembly Functions/_CSSRosterChange.cpp
@@ -37,20 +37,19 @@ void cssRosterChange() // Adapted from CSS Roster Change via Code Menu [QuickLav
 	// If CSS Rosters are enabled
 	if (CSS_VERSION_SETTING_INDEX != -1)
 	{
-		int reg1 = 6;
-		int reg2 = 5;
 		unsigned long charListAddress = 0x80680DE0;
 		unsigned long randListAddress = 0x80680E80;
 		unsigned long randomRangeStoreLocation = 0x800017C0;
-		unsigned long tempLLoadStoreLocation = randomRangeStoreLocation + 0x04;
-
-		ASMStart(0x80682928); // Hooks the second instruction of "__ct/[muSelCharTask]/mu_selchar.o".
-		SetRegister(reg1, CSS_VERSION_SETTING_INDEX); // Load the location of the CSS Roster Line into our first register.
-		LWZ(reg2, reg1, Line::VALUE); // Then Look 0x08 past that address to get the selected index of the CSS Roster Line
+		unsigned long temporaryRegBackupLocation = randomRangeStoreLocation + 0x04;
 
 		// There needs to at least be one pair of lists for every defined option in the Code Menu line!
 		assert(ROSTER_LIST.size() <= ROSTER_CHAR_ID_LISTS.size() && ROSTER_LIST.size() <= ROSTER_RAND_ID_LISTS.size());
 
+		int reg1 = 6;
+		int reg2 = 5;
+		ASMStart(0x80682928); // Hooks the second instruction of "__ct/[muSelCharTask]/mu_selchar.o".
+		SetRegister(reg1, CSS_VERSION_SETTING_INDEX); // Load the location of the CSS Roster Line into our first register.
+		LWZ(reg2, reg1, Line::VALUE); // Then Look 0x08 past that address to get the selected index of the CSS Roster Line
 		for (std::size_t i = 0; i < ROSTER_LIST.size(); i++) // For each Roster...
 		{
 			If(reg2, EQUAL_I, i); // ... add a case for that index...
@@ -70,6 +69,9 @@ void cssRosterChange() // Adapted from CSS Roster Change via Code Menu [QuickLav
 		LWZ(reg1, reg1, 0x00); // ... and load the number stored there into r23.
 		ASMEnd();
 
+
+		// Roster-Dependent L-Loads
+		// There needs to at least be one pair of L-Loads for every defined option in the Code Menu line!
 		assert(ROSTER_LIST.size() <= ROSTER_L_LOAD_PAIRS.size());
 
 		reg1 = 29;
@@ -97,7 +99,6 @@ void cssRosterChange() // Adapted from CSS Roster Change via Code Menu [QuickLav
 		SetRegister(reg2, 0);
 		ASMEnd();
 
-
 		reg1 = 5;
 		reg2 = 3;
 		ASMStart(0x80685BE4);
@@ -122,5 +123,38 @@ void cssRosterChange() // Adapted from CSS Roster Change via Code Menu [QuickLav
 		}
 		SetRegister(reg2, 0);
 		ASMEnd(0x90a1003c); // Restore original instruction (stw r5, 0x3C(r1))
+
+		reg1 = 3;
+		reg2 = 12;
+		int reg3 = 8;
+		ASMStart(0x806847BC);
+		SetRegister(reg2, temporaryRegBackupLocation); // Backup the contents of reg3
+		STW(reg3, reg2, 0x00);
+		SetRegister(reg3, 0x00);
+		SetRegister(reg2, CSS_VERSION_SETTING_INDEX); // Load the location of the CSS Roster Line into our first register.
+		LWZ(reg2, reg2, Line::VALUE); // Then Look 0x08 past that address to get the selected index of the CSS Roster Line
+		for (std::size_t i = 0; i < ROSTER_LIST.size(); i++) // For each Roster...
+		{
+			If(reg2, EQUAL_I, i); // ... add a case for that index...
+			{
+				// ... in which we check against each specified L-Load...
+				const std::vector<std::pair<unsigned char, unsigned char>>* currPairVec = &ROSTER_L_LOAD_PAIRS[i];
+				for (std::size_t u = 0; u < currPairVec->size(); u++)
+				{
+					// ... and if we have a match...
+					const std::pair<unsigned char, unsigned char>* currPair = &currPairVec->at(u);
+					If(reg1, EQUAL_I, currPair->first);
+					{
+						// ... set reg3 to 1.
+						SetRegister(reg3, 0x01);
+					}EndIf();
+				}
+			}EndIf();
+		}
+		// Check if reg3 was set, this is used once we step back out of the branch.
+		CMPI(reg3, 0x01, 0);
+		SetRegister(reg2, temporaryRegBackupLocation);
+		LWZ(reg3, reg2, 0x00); // Restore the contents of reg3
+		ASMEnd();
 	}
 }
